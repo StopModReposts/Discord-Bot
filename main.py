111800d39bae3094eb0b596ec59d5e2d78103ab1
Java sauce <mohammedbrandon204@gmail.com>
2022-02-25 14:24:59 -0400

    warn command

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from discord_slash import SlashCommand
 from dotenv import load_dotenv
 
 # to do :
-# add a warn system
 # multi ban
 
 # Basic setup
@@ -13,6 +12,7 @@ bot = commands.Bot(command_prefix='/', description=description, intents=discord.
 slash = SlashCommand(bot, sync_commands=True)
 # 785935453719101450 TESTING
 guild_ids = [463457129588850699]
+bot.warnings = {} # guild_id : {member_id: [count, [(admin_id, reason)]]}
 
 if os.path.exists(os.getcwd() + "/config.json"):
     
@@ -24,6 +24,84 @@ else:
 
     with open(os.getcwd() + "/config.json", "w+") as f:
         json.dump(configTemplate, f) 
+
+
+#warn command
+
+@bot.event
+async def on_ready():
+    for guild in bot.guilds:
+        bot.warnings[guild.id] = {}
+        
+        async with aiofiles.open(f"{guild.id}.txt", mode="a") as temp:
+            pass
+
+        async with aiofiles.open(f"{guild.id}.txt", mode="r") as file:
+            lines = await file.readlines()
+
+            for line in lines:
+                data = line.split(" ")
+                member_id = int(data[0])
+                admin_id = int(data[1])
+                reason = " ".join(data[2:]).strip("\n")
+
+                try:
+                    bot.warnings[guild.id][member_id][0] += 1
+                    bot.warnings[guild.id][member_id][1].append((admin_id, reason))
+
+                except KeyError:
+                    bot.warnings[guild.id][member_id] = [1, [(admin_id, reason)]] 
+    
+    print(bot.user.name + " is ready.")
+
+@bot.event
+async def on_guild_join(guild):
+    bot.warnings[guild.id] = {}
+
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def warn(ctx, member: discord.Member, *, reason=None):
+    if member is None:
+        return await ctx.send("the member could not be found")
+
+    if reason is None:
+        return await ctx.send("please provide a reason")
+
+    try:
+        first_warning = False
+        bot.warnings[ctx.guild.id][member.id][0] += 1
+        bot.warnings[ctx.guild.id][member.id][1].append((ctx.author.id, reason))
+    
+    except KeyError:
+        first_warning = True
+        bot.warnings[ctx.guild.id][member.id] = [1, [(ctx.author.id, reason)]]
+
+    count = bot.warnings[ctx.guild.id][member.id][0]
+
+    async with aiofiles.open(f"{ctx.guild.id}.txt", mode="a") as file:
+        await file.write(f"{member.id} {ctx.author.id} {reason}\n")
+
+    await ctx.send(f"{member.mention} has {count} {'warning' if first_warning else 'warnings'}.")
+
+
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def warnings(ctx, member: discord.Member=None):
+    if member is None:
+        return await ctx.send("The provided member could not be found or you forgot to provide one.")
+    
+    embed = discord.Embed(title=f"Displaying Warnings for {member.name}", description="", colour=discord.Colour.red())
+    try:
+        i = 1
+        for admin_id, reason in bot.warnings[ctx.guild.id][member.id][1]:
+            admin = ctx.guild.get_member(admin_id)
+            embed.description += f"**Warning {i}** given by: {admin.mention} for: *'{reason}'*.\n"
+            i += 1
+
+        await ctx.send(embed=embed)
+
+    except KeyError: # no warnings
+        await ctx.send("This user has no warnings.")
 
 
 #level  system
